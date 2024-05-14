f89b7ec8b0becc9c867ecb2205609a260d955d3d
mgrzanka <01186969@pw.edu.pl>
2024-05-14 11:39:59 +0200

    first attempt in client.hpp

--- Client/Client.hpp
+++ Client/Client.hpp
@@ -4,10 +4,36 @@
 
 
 #include "../Structures.hpp"
+#include "../Employee/Employee.hpp"
+#include "../Product/Product.hpp"
 
 
 class Client
-{};
+{
+    private:
+        // private attributes
+        std::string name;
+        std::string vorname;
+        Money money;
+        StoreTime time_spent;
+
+        // private, helping methods
+        Product pick_product();
+        Employee pick_employee();
+        void get_served_by_employee(Employee& picked_employee);
+        void update_money(Product& picked_product);
+
+    public:
+        // getters and setters
+        std::string get_name();
+        Money get_money();
+        StoreTime get_storetime();
+
+        // methods including client's iterraction with the store
+        void start_message();
+        void end_message();
+        void buy_somethig();
+};
 
 
 #endif
