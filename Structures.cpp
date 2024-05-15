71a7ebe2db71d819408e31dab22d63e818d1c739
mgrzanka <01186969@pw.edu.pl>
2024-05-15 16:48:41 +0200

    add constructors for structures StoreTime and Money, add -=operator and std::cout<< for Money

--- Structures.cpp
+++ Structures.cpp
@@ -1,21 +1,36 @@
-#include <iostream>
 #include <cmath>
 
 #include "Structures.hpp"
 
 
-
-unsigned int Money::get_whole_part()
+// Money structure
+unsigned int Money::get_whole_part() const
 {
     return int(full_price / 100);
 }
 
-unsigned int Money::get_cents()
+unsigned int Money::get_cents() const
 {
     return int(full_price % 100);
 }
 
-unsigned int StoreTime::get_iterations(unsigned int minutes)
+Money::Money(unsigned int full_price): full_price{full_price} {}
+std::ostream& operator<<(std::ostream& os, const Money& money)
+{
+    return os<<money.get_whole_part()<<"."<<money.get_cents()<<money.currency_name;
+}
+Money& operator-=(Money& first, const Money& second)
+{
+    unsigned int new_full_price = first.full_price - second.full_price;
+    first.full_price = new_full_price;
+    return first;
+}
+
+
+// StoreTime structure
+StoreTime::StoreTime(unsigned int minutes): minutes{minutes} {}
+
+unsigned int StoreTime::get_iterations() const
 {
     return static_cast<unsigned int>(ceil(minutes/minutes_per_iteration));
 }
