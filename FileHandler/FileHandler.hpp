6d3300dd4776c95969e66210cdffb56ab42883d6
mgrzanka <01186969@pw.edu.pl>
2024-05-22 14:26:56 +0200

    add file handler

---
+++ FileHandler/FileHandler.hpp
@@ -0,0 +1,16 @@
+#include "../Product/Product.hpp"
+#include "../Employee/Employee.hpp"
+#include "../Client/Client.hpp"
+#include <memory>
+#include <vector>
+
+
+
+class FileHandler
+{
+    std::string path;
+    public:
+        std::vector<std::unique_ptr<Client>> load_clients();
+        std::vector<std::unique_ptr<Employee>> load_employees();
+        std::vector<std::unique_ptr<Client>> load_products();
+};
