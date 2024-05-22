9edd5be44be29446e5f88c2d119eaa6221e3c988
Natasza Jarecka <01186970@pw.edu.pl>
2024-05-22 17:24:57 +0200

    added interface methods

---
+++ Employee/manager.hpp
@@ -0,0 +1,24 @@
+#ifndef MANAGER_HPP
+#define MANAGER_HPP
+#include "employee.hpp"
+
+
+class Manager : public Employee
+{
+    public:
+    Manager(std::string name, std::map<std::string, std::tuple<unsigned int, unsigned int>> weekly_schedule, Money bonus, Money base_hourly_wage);
+    Money calculate_weekly_salary() const override;
+    void tell_off_employee(std::string employee_name);
+    void praise_employee(std::string employee_name);
+
+    void give_rise(Employee& employee, const Money& rise);
+    void interaction_while_giving_rise(std::string employee_name);
+    void display_rise_message(std::string employee_name);
+
+    void start_message() const override;
+    void end_message() const override;
+
+
+};
+
+#endif
---
+++ Employee/storekeeper.hpp
@@ -0,0 +1,17 @@
+#ifndef STOREKEEPER_HPP
+#define STOREKEEPER_HPP
+#include "employee.hpp"
+
+class Storekeeper : public Employee
+{
+    public:
+    Storekeeper(std::string name, std::map<std::string, std::tuple<unsigned int, unsigned int>> weekly_schedule, Money bonus, Money base_hourly_wage);
+    Money calculate_weekly_salary() const override;
+    void replenish_stock(std::string product_name);
+    void display_replenishing_message(std::string product_name);
+
+    void start_message() const override;
+    void end_message() const override;
+
+};
+#endif
