f7ec259441e58b91b3b7c56b6a5474de8dc94c2a
mgrzanka <01186969@pw.edu.pl>
2024-05-22 14:34:05 +0200

    fix error in load_products - Client to Product type

--- FileHandler/FileHandler.hpp
+++ FileHandler/FileHandler.hpp
@@ -13,5 +13,5 @@ class FileHandler
         FileHandler(std::string path);
         std::vector<std::unique_ptr<Client>> load_clients();
         std::vector<std::unique_ptr<Employee>> load_employees();
-        std::vector<std::unique_ptr<Client>> load_products();
+        std::vector<std::unique_ptr<Product>> load_products();
 };
