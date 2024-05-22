88df1906ebbb5ee30e0145cc8e133b6a9ec10891
mgrzanka <01186969@pw.edu.pl>
2024-05-22 14:28:49 +0200

    add filehandler constrctor

--- FileHandler/FileHandler.hpp
+++ FileHandler/FileHandler.hpp
@@ -10,6 +10,7 @@ class FileHandler
 {
     std::string path;
     public:
+        FileHandler(std::string path);
         std::vector<std::unique_ptr<Client>> load_clients();
         std::vector<std::unique_ptr<Employee>> load_employees();
         std::vector<std::unique_ptr<Client>> load_products();
