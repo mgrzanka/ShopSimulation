8f082698a1f4f983cf9d008c3a535dc25f1b25b1
njarecka <01186970@pw.edu.pl>
2024-05-31 21:06:47 +0200

    brought back praise employee method

--- Employee/manager.cpp
+++ Employee/manager.cpp
@@ -3,23 +3,17 @@
 
 Manager::Manager(std::string new_name, std::map<std::string, std::tuple<unsigned int, unsigned int>> new_weekly_schedule, Money new_bonus, Money new_base_hourly_wage):
 Employee{new_name, new_weekly_schedule, new_bonus, new_base_hourly_wage}
-{
-    if (calculate_hours_worked()>36) {
-        throw std::invalid_argument("Managers cannot work more than 36 hours a week.");
-    }
-
-    for (auto pair : new_weekly_schedule) {
-        if (std::get<0>(pair.second) < 9 || std::get<0>(pair.second) > 20 ) {
-            throw std::invalid_argument("Managers can work only between 9AM and 8PM.");
-        }
-}
-}
+{}
 
 Money Manager::calculate_weekly_salary() const
 {
     return (base_hourly_wage + bonus + Money(20)) * calculate_hours_worked();
 }
 
+void Manager::praise_employee(std::string employee_name)
+{
+    employee_interface.print("Manager " + this->get_name() + ":" + employee_name + ", I'm really pleased with the work your're doing. Well done!\n");
+}
 void Manager::give_rise(Employee& employee, const Money& rise){
  interaction_while_giving_rise(employee.get_name());
  employee.set_bonus(employee.get_bonus() + rise);
@@ -29,7 +23,7 @@ void Manager::give_rise(Employee& employee, const Money& rise){
 
 void Manager::interaction_while_giving_rise(std::string employee_name)
 {
-    employee_interface.print("Manager " + this->get_name() + ":" + employee_name + "You've been doing your job really well lately. I'm happy to raise your hourly wage.\n");
+    employee_interface.print("Manager " + this->get_name() + ":" + employee_name + ", you've been doing your job really well lately. I'm happy to raise your hourly wage.\n");
 }
 
 
