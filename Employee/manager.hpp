6a8978aecfe04aabf89b669bc293e39865d00e24
njarecka <01186970@pw.edu.pl>
2024-05-31 17:50:17 +0200

    threw away two methods

--- Employee/manager.hpp
+++ Employee/manager.hpp
@@ -8,8 +8,6 @@ class Manager : public Employee
     public:
     Manager(std::string name, std::map<std::string, std::tuple<unsigned int, unsigned int>> weekly_schedule, Money bonus, Money base_hourly_wage);
     Money calculate_weekly_salary() const override;
-    void tell_off_employee(std::string employee_name);
-    void praise_employee(std::string employee_name);
 
     void give_rise(Employee& employee, const Money& rise);
     void interaction_while_giving_rise(std::string employee_name);
