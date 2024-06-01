6e49b39c4b790b8ca9faeebe2ddc8104308b76d6
njarecka <01186970@pw.edu.pl>
2024-06-01 16:18:32 +0200

    changed filenadler to read files with data separated with commas

--- FileHandler/FileHandler.cpp
+++ FileHandler/FileHandler.cpp
@@ -29,7 +29,7 @@ std::vector<std::tuple<std::string, std::string>> FileHandler::load_names()
         std::string name;
         std::string surname;
 
-        if (std::getline(lineStream, name, ' ') && std::getline(lineStream, surname)) {
+        if (std::getline(lineStream, name, ',') && std::getline(lineStream, surname)) {
             names.emplace_back(name, surname);
         }
     }
@@ -42,7 +42,32 @@ std::vector<std::tuple<std::string, std::string>> FileHandler::load_names()
 
 std::vector<std::unique_ptr<Product>> FileHandler::load_products()
 {
-   std::vector<std::unique_ptr<Product>> products;
+    file.open(path);
+    if (!file.is_open()) {
+        std::cerr << "Could not open the file!" << std::endl;
+    }
+    std::vector<std::tuple<std::string, std::string>> names;
+
+    std::string line;
+    while (std::getline(file, line)) {
+        line.erase(line.find_last_not_of(" \n\r\t")+1);
+        std::stringstream lineStream(line);
+        std::string name;
+        std::string surname;
+
+        if (std::getline(lineStream, name, ',') && std::getline(lineStream, surname)) {
+            names.emplace_back(name, surname);
+        }
+    }
+
+    file.close();
+    return names;
+}
+
+
+std::vector<std::unique_ptr<Product>> FileHandler::load_products()
+{
+    std::vector<std::unique_ptr<Product>> products;
     std::string line;
 
     file.open(path);
@@ -55,11 +80,15 @@ std::vector<std::unique_ptr<Product>> FileHandler::load_products()
         std::istringstream iss(line);
         std::string product_type, name, str_price, additional_attribute;
 
-        iss >> product_type >> name >> str_price >> additional_attribute;
+        std::getline(iss, product_type, ',');
+        std::getline(iss, name, ',');
+        std::getline(iss, str_price, ',');
+
         int int_price = std::stoi(str_price);
         Money price = Money(int_price);
 
         if (product_type == "fruit_vegetable") {
+            std::getline(iss, additional_attribute, ',');
             int amount_in_pack = std::stoi(additional_attribute);
             products.push_back(std::make_unique<FruitVegetable>(name, price, amount_in_pack));
 
@@ -67,14 +96,17 @@ std::vector<std::unique_ptr<Product>> FileHandler::load_products()
             products.push_back(std::make_unique<Beverage>(name, price));
 
         } else if (product_type == "breadstuff") {
+            std::getline(iss, additional_attribute, ',');
             int glycemic_index = std::stoi(additional_attribute);
             products.push_back(std::make_unique<Breadstuff>(name, price, glycemic_index));
 
         } else if (product_type == "cosmetics") {
+            std::getline(iss, additional_attribute, ',');
             char type = additional_attribute[0];
             products.push_back(std::make_unique<Cosmetics>(name, price, type));
         }
         else if (product_type == "dairy_product") {
+            std::getline(iss, additional_attribute, ',');
             double protein_content = std::stoi(additional_attribute);
             products.push_back(std::make_unique<DairyProduct>(name, price, protein_content));
 
@@ -87,5 +119,6 @@ std::vector<std::unique_ptr<Product>> FileHandler::load_products()
     }
 
     return products;
+
 
 }
