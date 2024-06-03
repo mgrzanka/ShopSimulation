3b46651578720d2b32a5e3d3d1a9e85af786e5cb
mgrzanka <01186969@pw.edu.pl>
2024-06-03 03:37:18 +0200

    add \n

--- Receipt/Receipt.cpp
+++ Receipt/Receipt.cpp
@@ -11,7 +11,7 @@ names {names}, prices {prices}, receipt_interface{}
 
 void Receipt::print_receipt()
 {
-    std::string header = "---- RECEIPT ------";
+    std::string header = "---- RECEIPT ------\n";
     std::string tp_ending = "---- total price: " + std::to_string(total_price.get_whole_part())+"."+std::to_string(total_price.get_cents())+total_price.currency_name+"\n";
     std::string no_tax_ending = "---- without tax: " + std::to_string(price_without_tax.get_whole_part())+"."+std::to_string(price_without_tax.get_cents())+price_without_tax.currency_name+"\n";
 
